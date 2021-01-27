658b3feb3a80e530431b683b3b6263c714bc2965
Liliia Pedorych <liliapedorych@ukr.net>
2021-01-27 22:02:57 +0200

    По нажатию на логотип/кнопку "Home" отрисовывается главная страница (популярные фильмы), делает home и library кнопками и сбрасивает их стили

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './main.scss';
 import './styles.css';
 
 import { getPopular, getPage, DataProccessing } from './js/apiServices';
+import refs from './js/refs';
+import galleryList from './js/updateGalleryMarkup';
 
 // import axios from './js/apiServices';
 
@@ -11,5 +13,17 @@ const dataProccessing = new DataProccessing();
 
 console.log(dataProccessing.getPopular());
 
-
-
+refs.logo.addEventListener('click', () => {
+  getPopular()
+    .then(data => data.results)
+    .then(data => {
+      galleryList(data);
+    });
+});
+refs.homeBtn.addEventListener('click', () => {
+  getPopular()
+    .then(data => data.results)
+    .then(data => {
+      galleryList(data);
+    });
+});
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+  filmGallery: document.querySelector('.film-gallery'),
+  logo: document.querySelector('.logo-block'),
+  homeBtn: document.querySelector('.home-btn'),
+};
+export default refs;
