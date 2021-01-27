e316eb9f0fa97e0fe1e76a241bd35b664d69017a
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2021-01-27 16:50:06 +0200

    Первая страница популярного, возможность перехода по страницам
    Добавляет пагинатор.
    Добавляет функциона по подгрузке первой страницы популярного + возможность перехода по страницам

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import 'normalize.css';
 import './main.scss';
 import './styles.css';
 
+import { getPopular, getPage, DataProccessing } from './js/apiServices';
+
 // import axios from './js/apiServices';
+
+const dataProccessing = new DataProccessing();
+
+console.log(dataProccessing.getPopular());
+
+
+
