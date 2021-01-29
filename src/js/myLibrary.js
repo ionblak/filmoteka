537171016940fb004f32c2a19c39e59c1889505a
wal1m <wal1m@yandex.ru>
2021-01-29 15:11:47 +0200

    При кликах по Home и My library отрисовуются нижнее подчеркивание

--- src/js/myLibrary.js
+++ src/js/myLibrary.js
@@ -1,11 +1,15 @@
-const { default: refs } = require("./refs")
+const { default: refs } = require('./refs');
 
 refs.libraryBtn.addEventListener('click', markLibrary);
 function markLibrary(event) {
-    event.preventDefault();
-    refs.filmsList.innerHTML = '';
-
+  event.preventDefault();
+  refs.filmsList.innerHTML = '';
+  refs.libraryBtn.classList.add('library-btn-active');
+  refs.homeBtn.classList.remove('home-btn-active');
 }
 
-
-// console.log(refs.libraryBtn);
+refs.homeBtn.addEventListener('click', markHome);
+function markHome(event) {
+  refs.homeBtn.classList.add('home-btn-active');
+  refs.libraryBtn.classList.remove('library-btn-active');
+}
