bc0e912b113ff3385d37834fc098443b11aca6ae
Igor Ionasku <ionaigor10@gmail.com>
2021-02-01 21:45:31 +0300

    добавил модалку

--- src/js/createCards.js
+++ src/js/createCards.js
@@ -1,7 +1,7 @@
 import itemsTemplate from '../templates/cardsTemplate.hbs';
 import modalCard from '../templates/modalCard.hbs';
-import * as basicLightbox from 'basiclightbox';
-import 'basiclightbox/dist/basicLightbox.min.css';
+// import * as basicLightbox from 'basiclightbox';
+// import 'basiclightbox/dist/basicLightbox.min.css';
 import refs from './refs';
 
 export default function createCards(data) {
@@ -17,11 +17,31 @@ export default function createCards(data) {
     if (currentCard.nodeName !== 'IMG') {
       return;
     }
+    refs.filmsListHome.innerHTML = '';
     const arrayIndex = currentCard.dataset.index;
-    // console.log(data.then);
 
     const markup = modalCard(data[arrayIndex]);
-    const modal = basicLightbox.create(markup);
-    modal.show();
+    refs.filmsListHome.innerHTML = markup;
+
+    window.addEventListener('keydown', closeModalHandler);
+    // document.addEventListener('click', closeModal);
+  }
+
+  function closeModalHandler(e) {
+    if (e.code === 'Escape') {
+      refs.filmsListHome.innerHTML = '';
+      const markup = itemsTemplate(data);
+      refs.filmsListHome.insertAdjacentHTML('beforeend', markup);
+      window.removeEventListener('keydown', closeModalHandler);
+    }
+  }
+
+  function closeModal(e) {
+    if (e.target !== refs.blockModalFilm) {
+      refs.filmsListHome.innerHTML = '';
+      const markup = itemsTemplate(data);
+      refs.filmsListHome.insertAdjacentHTML('beforeend', markup);
+      window.removeEventListener('keydown', closeModalHandler);
+    }
   }
 }
