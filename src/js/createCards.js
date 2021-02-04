b7beed9fc00d96298c3f6f80dbf4808010c8c05d
Igor Ionasku <ionaigor10@gmail.com>
2021-02-04 16:35:16 +0300

    обновил модалку

--- src/js/createCards.js
+++ src/js/createCards.js
@@ -26,62 +26,20 @@ export default function createCards(data) {
     if (currentCard.nodeName !== 'IMG') {
       return;
     }
-    // refs.filmsListHome.innerHTML = '';
+
     const arrayIndex = currentCard.dataset.index;
-    // refs.lightboxDiv.classList.add('is-open');
-    // refs.mainConteiner.classList.add('backdrop_close');
 
     const markup = modalCard(data[arrayIndex]);
-    // refs.lightboxDiv.innerHTML = markup;
+
     const instance = basicLightbox.create(markup);
     instance.show();
 
     addWatchedFilm();
     addFavoriteFilm();
 
-    // window.addEventListener('keydown', onEscapePress);
-    // document.addEventListener('click', closeModal);
+    window.addEventListener('keydown', () => {
+      instance.close();
+      window.removeEventListener('keydown');
+    });
   }
-
-  // refs.lightboxDiv.addEventListener('click', events => {
-  //   if (events.target === events.currentTarget) {
-  //     onCloseModal();
-  //   }
-  // });
-
-  // function onCloseModal() {
-  //   // refs.filmsListHome.innerHTML = '';
-  //   // const markup = itemsTemplate(data);
-  //   // refs.filmsListHome.insertAdjacentHTML('beforeend', markup);
-  //   window.removeEventListener('keydown', onEscapePress);
-  //   refs.lightboxDiv.classList.remove('is-open');
-  //   refs.mainConteiner.classList.remove('backdrop_close');
-  // }
-
-  // function onEscapePress(event) {
-  //   if (event.code === 'Escape') {
-  //     onCloseModal();
-  //   }
-  // }
 }
-
-// function closeModalHandler(e) {
-//   // if (e.code === 'Escape') {
-//     refs.filmsListHome.innerHTML = '';
-//     const markup = itemsTemplate(data);
-//     refs.filmsListHome.insertAdjacentHTML('beforeend', markup);
-//     window.removeEventListener('keydown', closeModalHandler);
-//     refs.mainConteiner.classList.remove("backdrop_close");
-//     refs.lightboxDiv.classList.remove("is-open");
-//   // }
-
-// }
-
-// function closeModal(e) {
-//   if (e.target !== refs.blockModalFilm) {
-//     refs.filmsListHome.innerHTML = '';
-//     const markup = itemsTemplate(data);
-//     refs.filmsListHome.insertAdjacentHTML('beforeend', markup);
-//     window.removeEventListener('keydown', closeModalHandler);
-//   }
-// }
