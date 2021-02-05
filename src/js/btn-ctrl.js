cda62ff8461d07287910af08d442c0b3d17731d8
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2021-02-06 01:35:16 +0200

    Добавляет фичи - кнопку вверх экрана, удаление из списков
    - добавляет кнопку для перехода вверх страницы от футера, стили в _footer.scss функционал для перехода в index.js
    - _modalFilm.scss добавляет доп классы для анимации кнопок добавления / удаления в списки. А так же стилизует рамку кнопки по макету
    - btn-ctrl.js сожержит функцию для изменения текста кнопки + анимацию перехода

--- src/js/btn-ctrl.js
+++ src/js/btn-ctrl.js
@@ -1,41 +1,48 @@
-
 export function changeBtnText(list, mode) {
     if (list === 'watched') {
         const refWatchedBtn = document.querySelector('.btn-watched');
         if (mode === 'remove') refWatchedBtn.innerHTML = `REMOVE FROM WATCHED`;
-        else if (mode === 'add') refWatchedBtn.innerHTML = `ADD TO WATCHED`;
+        else if (mode === 'add') refWatchedBtn.innerHTML = `ADD TO WATCHED`;   
+        textAnimation(refWatchedBtn);
     } else if (list === 'favorite'){
         const refWatchedBtn = document.querySelector('.btn-favorite');
         if (mode === 'remove') refWatchedBtn.innerHTML = `REMOVE FROM QUEUE`;
         else if (mode === 'add') refWatchedBtn.innerHTML = `ADD TO QUEUE`;
+        textAnimation(refWatchedBtn);
+    }    
+}
+
+
+function textAnimation(btnRef) {        
+    const strText = btnRef.textContent;
+    const splitText = strText.split("");
+    btnRef.textContent = "";
+    for (let i = 0; i < splitText.length; i++) {
+        btnRef.innerHTML += "<span>" + splitText[i] + "</span>";
     }
+    let char = 0;
+    let timer = setInterval(onTick, 30);
     
+    function onTick() {
+        const span = btnRef.querySelectorAll('span')[char];
+        span.classList.add('fade');
+        char++
+        if (char === splitText.length) {
+            complete();
+            return;
+        }
+    }
+
+    function complete() {
+        clearInterval(timer);
+        timer = null;
+        const span =  Array.from(btnRef.querySelectorAll('span'));
+        const spanSplit = span.map(item => item.innerHTML);
+        btnRef.innerHTML = spanSplit.join('');
+    }
 }
 
 
 
-const text = document.querySelector(".fancy");
-const strText = text.textContent;
-const splitText = strText.split("");
-text.textContent = "";
-for(let i = 0; i < splitText.length; i++) {
-  text.innerHTML += "<span>" + splitText[i] + "</span>";
-}
 
-let char = 0;
-let timer = setInterval(onTick, 50);
 
-function onTick(){
-  const span = text.querySelectorAll('span')[char];
-  span.classList.add('fade');
-  char++ 
-  if(char === splitText.length){
-    complete();
-    return;
-  }
-}
-
-function complete(){
-  clearInterval(timer);
-  timer = null;
-}
