62e364ffa982c0763af375dec0a7123240408978
Anton Chyhariev <tonichiga@gmail.com>
2021-02-10 02:24:25 +0200

    Добавляет ссылки в модалке команды

--- src/js/our-students.js
+++ src/js/our-students.js
@@ -1,3 +1,5 @@
+import sprite from '../images/sprite1.svg';
+
 export const markupStudents = img =>
   ` <div class="container slider">
     <div class="slider-wrapper">
@@ -15,11 +17,50 @@ export const markupStudents = img =>
 
           </div>
           <p class="slider-item-name">Igor Ionashku</p>
-          <div class="wrapper">
+          <div class="wrapper slider-text">
             <p class="slider-text">
               Скрывался за тайной космического фона:) </br> Team Lead
             </p>
         
+        <ul class="social-list">
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://github.com/ionblak/"
+                    aria-label="GitHub">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#github"></use>
+                    </svg>
+        </a>
+        </li>
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.instagram.com/igorionashky?r=nametag"
+                    aria-label="Instagram">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-insta"></use>
+                    </svg>
+        </a>
+        </li>
+         <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.linkedin.com/in/igor-ionashku-b271b21b3"
+                    aria-label="LinkedIn">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-linkedin"></use>
+                    </svg>
+        </a>
+        </li>
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.facebook.com/igor.ionashky"
+                    aria-label="Facebook">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-facebook"></use>
+                    </svg>
+        </a>
+        </li>
+        </ul>
+      
           </div>
         </li>
 
@@ -35,14 +76,99 @@ export const markupStudents = img =>
            
           </div>
           <p class="slider-item-name">Neliia Zelienova</p>
-          <div class="wrapper">
+          <div class="wrapper slider-text">
             <p class="slider-text">
               Вышла один на один с api - сервисами и нокаутировала их!:) </br> Scrum Master
             </p>
+         <ul class="social-list">
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://github.com/neliia-zelienova"
+                    aria-label="GitHub">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#github"></use>
+                    </svg>
+        </a>
+        </li>
        
+         <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.linkedin.com/in/neliia-zelienova-208b2b137/"
+                    aria-label="LinkedIn">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-linkedin"></use>
+                    </svg>
+        </a>
+        </li>
+        <li class="social-item">
+                  <a class="icon-container-link footer-social"
+                    href="https://www.facebook.com/profile.php?id=100017462564897y"
+                    aria-label="Facebook">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-facebook"></use>
+                    </svg>
+                  </a>
+                </li>
+            </ul>
           </div>
         </li>
 
+
+         <li class="slider-item">
+          <div class="picture-wrapper">
+        
+              <img
+                class="sim-slider-element"
+               
+                src="./images/students/anton.png"
+                alt="фото клиента"
+              />
+          </div>
+          <p class="slider-item-name">Anton Chyhariev</p>
+          <div class="wrapper slider-text">
+            <p class="slider-text">
+             Всё время делал вид, что чем-то очень сильно занят) </br> Warrior
+            </p>
+            <ul class="social-list">
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://github.com/tonichiga"
+                    aria-label="GitHub">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#github"></use>
+                    </svg>
+        </a>
+        </li>
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.instagram.com/toni_chiga/"
+                    aria-label="Instagram">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-insta"></use>
+                    </svg>
+        </a>
+        </li>
+         <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.linkedin.com/in/anton-chyhariev-2a5061203/"
+                    aria-label="LinkedIn">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-linkedin"></use>
+                    </svg>
+        </a>
+        </li>
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.facebook.com/antoxabomba"
+                    aria-label="Facebook">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-facebook"></use>
+                    </svg>
+        </a>
+        </li>
+        </ul>
+          </div>
+        </li>
         <li class="slider-item">
           <div class="picture-wrapper">
 
@@ -55,11 +181,22 @@ export const markupStudents = img =>
 
           </div>
           <p class="slider-item-name">Olga Brekhuntsova</p>
-          <div class="wrapper">
+          <div class="wrapper slider-text">
             <p class="slider-text">
              Порвала пагинатор, как Тузик - грелку!) </br> Warrior
             </p>
-        
+         <ul class="social-list">
+      
+        <li class="social-item">
+          <a class="icon-container-link footer-social"
+                    href="https://www.facebook.com/profile.php?id=100017462564897y"
+                    aria-label="Facebook">
+                    <svg class="footer-icon-social-svg">
+                      <use href="${sprite}#icon-facebook"></use>
+                    </svg>
+        </a>
+        </li>
+        </ul>
           </div>
         </li>
 
@@ -83,24 +220,7 @@ export const markupStudents = img =>
           </div>
         </li>
 
-        <li class="slider-item">
-          <div class="picture-wrapper">
-        
-              <img
-                class="sim-slider-element"
-               
-                src="./images/students/anton.png"
-                alt="фото клиента"
-              />
-          </div>
-          <p class="slider-item-name">Anton Chyhariev</p>
-          <div class="wrapper">
-            <p class="slider-text">
-             Всё время делал вид, что чем-то очень сильно занят) </br> Warrior
-            </p>
-           
-          </div>
-        </li>
+       
 
         <li class="slider-item">
           <div class="picture-wrapper">
@@ -179,4 +299,4 @@ export const markupStudents = img =>
         <li class="slider-dots_item" data-onclick="8"></li>
       </ul>
     </div>
-  </div>`;
+  </div> `;
